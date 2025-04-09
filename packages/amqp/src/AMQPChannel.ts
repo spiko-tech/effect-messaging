e3bb8b9f1ab566f84011ded472f139f543112e0a
Samuel Briole <samuel.briole@gmail.com>
2025-04-09 11:39:25 +0200

    improve imports a bit more

--- packages/amqp/src/AMQPChannel.ts
+++ packages/amqp/src/AMQPChannel.ts
@@ -7,8 +7,8 @@ import * as Effect from "effect/Effect"
 import * as Layer from "effect/Layer"
 import type * as Scope from "effect/Scope"
 import type * as Stream from "effect/Stream"
-import type { AMQPConnection } from "./AMQPConnection.js"
-import type { AMQPChannelError, AMQPConnectionError } from "./AMQPError.js"
+import type * as AMQPConnection from "./AMQPConnection.js"
+import type * as AMQPError from "./AMQPError.js"
 import * as internal from "./internal/AMQPChannel.js"
 
 /**
@@ -29,25 +29,27 @@ export type TypeId = typeof TypeId
  */
 export interface AMQPChannel {
   readonly [TypeId]: TypeId
-  readonly consume: (queueName: string) => Stream.Stream<ConsumeMessage, AMQPChannelError>
-  readonly ack: (...parameters: Parameters<Channel["ack"]>) => Effect.Effect<void, AMQPChannelError>
-  readonly nack: (...parameters: Parameters<Channel["nack"]>) => Effect.Effect<void, AMQPChannelError>
-  readonly publish: (...parameters: Parameters<Channel["publish"]>) => Effect.Effect<boolean, AMQPChannelError>
+  readonly consume: (queueName: string) => Stream.Stream<ConsumeMessage, AMQPError.AMQPChannelError>
+  readonly ack: (...parameters: Parameters<Channel["ack"]>) => Effect.Effect<void, AMQPError.AMQPChannelError>
+  readonly nack: (...parameters: Parameters<Channel["nack"]>) => Effect.Effect<void, AMQPError.AMQPChannelError>
+  readonly publish: (
+    ...parameters: Parameters<Channel["publish"]>
+  ) => Effect.Effect<boolean, AMQPError.AMQPChannelError>
   readonly assertQueue: (
     ...parameters: Parameters<Channel["assertQueue"]>
-  ) => Effect.Effect<Replies.AssertQueue, AMQPChannelError>
+  ) => Effect.Effect<Replies.AssertQueue, AMQPError.AMQPChannelError>
   readonly bindQueue: (
     ...parameters: Parameters<Channel["bindQueue"]>
-  ) => Effect.Effect<Replies.Empty, AMQPChannelError>
+  ) => Effect.Effect<Replies.Empty, AMQPError.AMQPChannelError>
   readonly unbindQueue: (
     ...parameters: Parameters<Channel["unbindQueue"]>
-  ) => Effect.Effect<Replies.Empty, AMQPChannelError>
+  ) => Effect.Effect<Replies.Empty, AMQPError.AMQPChannelError>
   readonly assertExchange: (
     ...parameters: Parameters<Channel["assertExchange"]>
-  ) => Effect.Effect<Replies.AssertExchange, AMQPChannelError>
+  ) => Effect.Effect<Replies.AssertExchange, AMQPError.AMQPChannelError>
   readonly checkExchange: (
     ...parameters: Parameters<Channel["checkExchange"]>
-  ) => Effect.Effect<Replies.Empty, AMQPChannelError>
+  ) => Effect.Effect<Replies.Empty, AMQPError.AMQPChannelError>
 }
 
 /**
@@ -60,50 +62,53 @@ export const AMQPChannel = Context.GenericTag<AMQPChannel>("@effect-messaging/am
  * @category constructors
  * @since 0.1.0
  */
-export const make: Effect.Effect<AMQPChannel, AMQPChannelError | AMQPConnectionError, Scope.Scope | AMQPConnection> =
-  Effect.gen(
-    function*() {
-      const channel = yield* Effect.acquireRelease(
-        Effect.gen(function*() {
-          const channelRef = yield* internal.ChannelRef.make()
-          yield* internal.initiateChannel(channelRef)
-          return {
-            [TypeId]: TypeId as TypeId,
-            consume: (queueName: string) => internal.consume(channelRef, queueName),
-            ack: (...params: Parameters<Channel["ack"]>) =>
-              internal.wrapChannelMethod(channelRef, "ack", async (channel) => channel.ack(...params)),
-            nack: (...params: Parameters<Channel["nack"]>) =>
-              internal.wrapChannelMethod(channelRef, "nack", async (channel) => channel.nack(...params)),
-            publish: (...params: Parameters<Channel["publish"]>) =>
-              internal.wrapChannelMethod(channelRef, "publish", async (channel) => channel.publish(...params)),
-            assertQueue: (...params: Parameters<Channel["assertQueue"]>) =>
-              internal.wrapChannelMethod(channelRef, "assertQueue", async (channel) => channel.assertQueue(...params)),
-            bindQueue: (...params: Parameters<Channel["bindQueue"]>) =>
-              internal.wrapChannelMethod(channelRef, "bindQueue", async (channel) => channel.bindQueue(...params)),
-            unbindQueue: (...params: Parameters<Channel["unbindQueue"]>) =>
-              internal.wrapChannelMethod(channelRef, "unbindQueue", async (channel) => channel.unbindQueue(...params)),
-            assertExchange: (...params: Parameters<Channel["assertExchange"]>) =>
-              internal.wrapChannelMethod(
-                channelRef,
-                "assertExchange",
-                async (channel) => channel.assertExchange(...params)
-              ),
-            checkExchange: (...params: Parameters<Channel["checkExchange"]>) =>
-              internal.wrapChannelMethod(
-                channelRef,
-                "checkExchange",
-                async (channel) => channel.checkExchange(...params)
-              ),
-            close: internal.closeChannel(channelRef),
-            watchChannel: internal.watchChannel(channelRef)
-          }
-        }),
-        (channel) => channel.close
-      )
-      yield* Effect.forkScoped(channel.watchChannel)
-      return channel
-    }
-  )
+export const make: Effect.Effect<
+  AMQPChannel,
+  AMQPError.AMQPChannelError | AMQPError.AMQPConnectionError,
+  Scope.Scope | AMQPConnection.AMQPConnection
+> = Effect.gen(
+  function*() {
+    const channel = yield* Effect.acquireRelease(
+      Effect.gen(function*() {
+        const channelRef = yield* internal.ChannelRef.make()
+        yield* internal.initiateChannel(channelRef)
+        return {
+          [TypeId]: TypeId as TypeId,
+          consume: (queueName: string) => internal.consume(channelRef, queueName),
+          ack: (...params: Parameters<Channel["ack"]>) =>
+            internal.wrapChannelMethod(channelRef, "ack", async (channel) => channel.ack(...params)),
+          nack: (...params: Parameters<Channel["nack"]>) =>
+            internal.wrapChannelMethod(channelRef, "nack", async (channel) => channel.nack(...params)),
+          publish: (...params: Parameters<Channel["publish"]>) =>
+            internal.wrapChannelMethod(channelRef, "publish", async (channel) => channel.publish(...params)),
+          assertQueue: (...params: Parameters<Channel["assertQueue"]>) =>
+            internal.wrapChannelMethod(channelRef, "assertQueue", async (channel) => channel.assertQueue(...params)),
+          bindQueue: (...params: Parameters<Channel["bindQueue"]>) =>
+            internal.wrapChannelMethod(channelRef, "bindQueue", async (channel) => channel.bindQueue(...params)),
+          unbindQueue: (...params: Parameters<Channel["unbindQueue"]>) =>
+            internal.wrapChannelMethod(channelRef, "unbindQueue", async (channel) => channel.unbindQueue(...params)),
+          assertExchange: (...params: Parameters<Channel["assertExchange"]>) =>
+            internal.wrapChannelMethod(
+              channelRef,
+              "assertExchange",
+              async (channel) => channel.assertExchange(...params)
+            ),
+          checkExchange: (...params: Parameters<Channel["checkExchange"]>) =>
+            internal.wrapChannelMethod(
+              channelRef,
+              "checkExchange",
+              async (channel) => channel.checkExchange(...params)
+            ),
+          close: internal.closeChannel(channelRef),
+          watchChannel: internal.watchChannel(channelRef)
+        }
+      }),
+      (channel) => channel.close
+    )
+    yield* Effect.forkScoped(channel.watchChannel)
+    return channel
+  }
+)
 
 /**
  * @since 0.1.0
@@ -111,6 +116,6 @@ export const make: Effect.Effect<AMQPChannel, AMQPChannelError | AMQPConnectionE
  */
 export const layer: Layer.Layer<
   AMQPChannel,
-  AMQPChannelError | AMQPConnectionError,
-  AMQPConnection
+  AMQPError.AMQPChannelError | AMQPError.AMQPConnectionError,
+  AMQPConnection.AMQPConnection
 > = Layer.scoped(AMQPChannel, make)

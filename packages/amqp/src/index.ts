8b51b75f9010f0417ae1d00912d43455d9e1f36b
Samuel Briole <samuel.briole@gmail.com>
2025-04-09 19:21:28 +0200

    Implement AMQPPublisher

--- packages/amqp/src/index.ts
+++ packages/amqp/src/index.ts
@@ -12,3 +12,8 @@ export * as AMQPConnection from "./AMQPConnection.js"
  * @since 0.1.0
  */
 export * as AMQPError from "./AMQPError.js"
+
+/**
+ * @since 0.3.0
+ */
+export * as AMQPPublisher from "./AMQPPublisher.js"

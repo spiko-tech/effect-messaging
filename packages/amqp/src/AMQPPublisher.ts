331be3e1eaf77af8a917f26c0840eb3403ec502b
Samuel Briole <samuel.briole@gmail.com>
2025-04-09 19:21:28 +0200

    Implement AMQPPublisher

---
+++ packages/amqp/src/AMQPPublisher.ts
@@ -0,0 +1,67 @@
+/**
+ * @since 0.3.0
+ */
+import * as Publisher from "@effect-messaging/publisher/Publisher"
+import * as PublisherError from "@effect-messaging/publisher/PublisherError"
+import type { Options } from "amqplib"
+import * as Effect from "effect/Effect"
+import { AMQPChannel } from "./AMQPChannel.js"
+
+/**
+ * @category type ids
+ * @since 0.3.0
+ */
+export const TypeId: unique symbol = Symbol.for("@effect-messaging/amqp/AMQPPublisher")
+
+/**
+ * @category type ids
+ * @since 0.3.0
+ */
+export type TypeId = typeof TypeId
+
+/**
+ * @category models
+ * @since 0.3.0
+ */
+export interface AMQPPublishMessage {
+  exchange: string
+  routingKey: string
+  content: Buffer
+  options?: Options.Publish
+}
+
+/**
+ * @category models
+ * @since 0.3.0
+ */
+export interface AMQPPublisher extends Publisher.Publisher<AMQPPublishMessage> {
+  readonly [TypeId]: TypeId
+}
+
+/* @internal */
+const publish =
+  (channel: AMQPChannel) => (message: AMQPPublishMessage): Effect.Effect<void, PublisherError.PublisherError, never> =>
+    channel.publish(message.exchange, message.routingKey, message.content, message.options).pipe(
+      Effect.catchTag(
+        "AMQPChannelError",
+        (error) => Effect.fail(new PublisherError.PublisherError({ reason: "Failed to publish message", cause: error }))
+      ),
+      Effect.map(() => undefined)
+    )
+
+/**
+ * @category constructors
+ * @since 0.3.0
+ */
+export const make = (): Effect.Effect<AMQPPublisher, never, AMQPChannel> =>
+  Effect.gen(function*() {
+    const channel = yield* AMQPChannel
+
+    const publisher: AMQPPublisher = {
+      [TypeId]: TypeId,
+      [Publisher.TypeId]: Publisher.TypeId,
+      publish: publish(channel)
+    }
+
+    return publisher
+  })
--- packages/publisher/src/index.ts
+++ packages/publisher/src/index.ts
@@ -1,4 +1,9 @@
 /**
  * @since 0.3.0
  */
+export * as Publisher from "./Publisher.js"
+
+/**
+ * @since 0.3.0
+ */
 export * as PublisherError from "./PublisherError.js"

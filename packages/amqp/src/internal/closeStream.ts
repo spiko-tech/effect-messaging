7c28167b125572ff208f27d17ae616536015472f
Samuel Briole <samuel.briole@gmail.com>
2025-04-24 12:43:32 +0200

    add error monitoring on AMQPChannel and AMQPConnection

--- packages/amqp/src/internal/closeStream.ts
+++ packages/amqp/src/internal/closeStream.ts
@@ -3,20 +3,27 @@ import type { SubscriptionRef } from "effect"
 import { Effect, Option, Stream } from "effect"
 
 /** @internal */
-export const closeStream = <T extends Connection | Channel>(ref: SubscriptionRef.SubscriptionRef<Option.Option<T>>) =>
-  ref.changes.pipe(
-    Stream.flatMap(
-      (target) => {
-        if (Option.isNone(target)) {
-          return Stream.never
-        } else {
-          return Stream.asyncPush<unknown>((emit) =>
-            Effect.sync(() => {
-              target.value.addListener("close", emit.single)
-            })
-          )
-        }
-      },
-      { concurrency: "unbounded" }
+const eventStream =
+  (eventName: string) => <T extends Connection | Channel>(ref: SubscriptionRef.SubscriptionRef<Option.Option<T>>) =>
+    ref.changes.pipe(
+      Stream.flatMap(
+        (target) => {
+          if (Option.isNone(target)) {
+            return Stream.never
+          } else {
+            return Stream.asyncPush<unknown>((emit) =>
+              Effect.sync(() => {
+                target.value.addListener(eventName, emit.single)
+              })
+            )
+          }
+        },
+        { concurrency: "unbounded" }
+      )
     )
-  )
+
+/** @internal */
+export const closeStream = eventStream("close")
+
+/** @internal */
+export const errorStream = eventStream("error")

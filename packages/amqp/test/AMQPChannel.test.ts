8ec166a2caa27a3f7112d9e0a7e36cd249a353ec
Samuel Briole <samuel.briole@gmail.com>
2025-04-25 12:12:54 +0200

    add test on checkQueue

--- packages/amqp/test/AMQPChannel.test.ts
+++ packages/amqp/test/AMQPChannel.test.ts
@@ -1,7 +1,14 @@
 import { describe, expect, it, layer } from "@effect/vitest"
-import { Effect, TestServices } from "effect"
+import { Effect, Exit, TestServices } from "effect"
 import * as AMQPChannel from "../src/AMQPChannel.js"
-import { assertTestExchange, simulateChannelClose, simulateConnectionClose, testChannel } from "./dependencies.js"
+import { AMQPChannelError } from "../src/AMQPError.js"
+import {
+  assertTestExchange,
+  assertTestQueue,
+  simulateChannelClose,
+  simulateConnectionClose,
+  testChannel
+} from "./dependencies.js"
 
 describe("AMQPChannel", () => {
   layer(testChannel)("connection", (it) => {
@@ -38,6 +45,25 @@ describe("AMQPChannel", () => {
 
         // should wait for channel to re-open and assert exchange
         yield* assertTestExchange
+      }).pipe(Effect.provide(testChannel), TestServices.provideLive))
+  })
+
+  describe("checkQueue", () => {
+    it.effect("Should return a successful assertion of the queue", () =>
+      Effect.gen(function*() {
+        yield* assertTestQueue
+        const channel = yield* AMQPChannel.AMQPChannel
+        const result = yield* channel.checkQueue("TEST_QUEUE")
+        expect(result).toMatchObject({
+          queue: "TEST_QUEUE"
+        })
+      }).pipe(Effect.provide(testChannel), TestServices.provideLive))
+
+    it.effect("Should return an error when the queue does not exist", () =>
+      Effect.gen(function*() {
+        const channel = yield* AMQPChannel.AMQPChannel
+        const exit = yield* channel.checkQueue("NON_EXISTENT_QUEUE").pipe(Effect.exit)
+        expect(exit).toStrictEqual(Exit.fail(expect.any(AMQPChannelError)))
       }).pipe(Effect.provide(testChannel), TestServices.provideLive))
   })
 })

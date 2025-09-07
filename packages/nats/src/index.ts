54b676c98810129f29fc98271c2bb6cdb435871e
Samuel Briole <samuel.briole@gmail.com>
2025-09-08 00:02:45 +0200

    run codegen

--- packages/nats/src/index.ts
+++ packages/nats/src/index.ts
@@ -7,3 +7,13 @@ export * as NATSConnection from "./NATSConnection.js"
  * @since 0.1.0
  */
 export * as NATSError from "./NATSError.js"
+
+/**
+ * @since 0.1.0
+ */
+export * as NATSJetStreamClient from "./NATSJetStreamClient.js"
+
+/**
+ * @since 0.1.0
+ */
+export * as NATSJetStreamManager from "./NATSJetStreamManager.js"

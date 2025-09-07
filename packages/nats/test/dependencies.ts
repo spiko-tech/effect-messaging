849dabf1fedf08310cf9607d6aec28f08d9003ab
Samuel Briole <samuel.briole@gmail.com>
2025-09-07 23:42:42 +0200

    add NATSJetStreamClient and NATSJetStreamManager

--- packages/nats/test/dependencies.ts
+++ packages/nats/test/dependencies.ts
@@ -1,5 +1,12 @@
+import { Layer } from "effect"
 import * as NATSConnection from "../src/NATSConnection.js"
+import * as NATSJetStreamClient from "../src/NATSJetStreamClient.js"
+import * as NATSJetStreamManager from "../src/NATSJetStreamManager.js"
 
 export const testConnection = NATSConnection.layerNode({
   servers: "localhost:4222"
 })
+
+export const testJetStreamClient = NATSJetStreamClient.layer().pipe(Layer.provideMerge(testConnection))
+
+export const testJetStreamManager = NATSJetStreamManager.layer().pipe(Layer.provideMerge(testConnection))

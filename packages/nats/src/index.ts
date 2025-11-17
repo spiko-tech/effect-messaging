8923b409e5a6a1b2bf9da7a6446d138a8e969601
Samuel Briole <samuel.briole@gmail.com>
2025-11-17 17:15:10 +0100

    add NATSSubscription module

--- packages/nats/src/index.ts
+++ packages/nats/src/index.ts
@@ -17,3 +17,13 @@ export * as NATSJetStreamClient from "./NATSJetStreamClient.js"
  * @since 0.1.0
  */
 export * as NATSJetStreamManager from "./NATSJetStreamManager.js"
+
+/**
+ * @since 0.1.0
+ */
+export * as NATSMessage from "./NATSMessage.js"
+
+/**
+ * @since 0.1.0
+ */
+export * as NATSSubscription from "./NATSSubscription.js"
--- packages/nats/test/NATSConnection.test.ts
+++ packages/nats/test/NATSConnection.test.ts
@@ -1,14 +1,45 @@
-import { describe, expect, layer } from "@effect/vitest"
-import { Effect, Option } from "effect"
+import { describe, expect, layer, vi } from "@effect/vitest"
+import { Effect, Option, Stream, TestServices } from "effect"
 import * as NATSConnection from "../src/NATSConnection.js"
+import type * as NATSMessage from "../src/NATSMessage.js"
 import { testConnection } from "./dependencies.js"
 
-describe("NATSConnection", () => {
-  layer(testConnection)((it) => {
+layer(testConnection)("NATSConnection", (it) => {
+  describe("info", () => {
     it.effect("Should be able to connect", () =>
       Effect.gen(function*() {
         const connection = yield* NATSConnection.NATSConnection
         expect(connection.info).toMatchObject(Option.some({ port: 4222 }))
       }))
+  })
+
+  describe("publish/subscribe", () => {
+    it.effect("Should be able to publish and subscribe to a subject", () =>
+      Effect.gen(function*() {
+        const connection = yield* NATSConnection.NATSConnection
+
+        const subject = "test.subject"
+        const message = "Hello, NATS!"
+
+        const subscription = yield* connection.subscribe(subject)
+
+        const onMessage = vi.fn<(message: NATSMessage.NATSMessage) => void>()
+        const messageHandler = (message: NATSMessage.NATSMessage) =>
+          Effect.gen(function*() {
+            onMessage(message)
+            return yield* Effect.void
+          })
+
+        yield* Effect.fork(subscription.stream.pipe(Stream.runForEach(messageHandler)))
+
+        yield* connection.publish(subject, message)
+
+        // Wait for the message to be consumed
+        yield* Effect.sleep("100 millis")
+
+        expect(onMessage).toHaveBeenCalledTimes(1)
+        const receivedMessage = onMessage.mock.calls[0][0]
+        expect(yield* receivedMessage.string).toStrictEqual("Hello, NATS!")
+      }).pipe(Effect.provide(testConnection), TestServices.provideLive), { timeout: 5000 })
   })
 })

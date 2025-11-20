73815dcddc67a60052464195751fff90341d6a9a
Samuel Briole <samuel.briole@gmail.com>
2025-11-20 17:23:51 +0100

    add JetStreamLister and NATSQueuedIterator wrapper modules with typed error handling

--- packages/nats/src/JetStreamDirectStreamAPI.ts
+++ packages/nats/src/JetStreamDirectStreamAPI.ts
@@ -2,13 +2,12 @@
  * @since 0.1.0
  */
 import type * as JetStream from "@nats-io/jetstream"
-import * as Context from "effect/Context"
 import * as Effect from "effect/Effect"
 import * as Option from "effect/Option"
-import * as Stream from "effect/Stream"
 import * as utils from "./internal/utils.js"
 import * as JetStreamStoredMessage from "./JetStreamStoredMessage.js"
 import * as NATSError from "./NATSError.js"
+import * as NATSQueuedIterator from "./NATSQueuedIterator.js"
 
 /**
  * @category type ids
@@ -39,22 +38,22 @@ export interface JetStreamDirectStreamAPI {
   >
   readonly getBatch: (
     ...params: Parameters<JetStream.DirectStreamAPI["getBatch"]>
-  ) => Stream.Stream<JetStreamStoredMessage.JetStreamStoredMessage, NATSError.JetStreamDirectStreamAPIError, never>
+  ) => Effect.Effect<
+    NATSQueuedIterator.NATSQueuedIterator<JetStream.StoredMsg, NATSError.JetStreamDirectStreamAPIError>,
+    NATSError.JetStreamDirectStreamAPIError,
+    never
+  >
   readonly getLastMessagesFor: (
     ...params: Parameters<JetStream.DirectStreamAPI["getLastMessagesFor"]>
-  ) => Stream.Stream<JetStreamStoredMessage.JetStreamStoredMessage, NATSError.JetStreamDirectStreamAPIError, never>
+  ) => Effect.Effect<
+    NATSQueuedIterator.NATSQueuedIterator<JetStream.StoredMsg, NATSError.JetStreamDirectStreamAPIError>,
+    NATSError.JetStreamDirectStreamAPIError,
+    never
+  >
 
   /** @internal */
   readonly direct: JetStream.DirectStreamAPI
 }
-
-/**
- * @category tags
- * @since 0.1.0
- */
-export const JetStreamDirectStreamAPI = Context.GenericTag<JetStreamDirectStreamAPI>(
-  "@effect-messaging/nats/JetStreamDirectStreamAPI"
-)
 
 const wrapAsync = utils.wrapAsync(NATSError.JetStreamDirectStreamAPIError)
 
@@ -71,33 +70,11 @@ export const make = (direct: JetStream.DirectStreamAPI): JetStreamDirectStreamAP
     ),
   getBatch: (...params: Parameters<JetStream.DirectStreamAPI["getBatch"]>) =>
     wrapAsync(() => direct.getBatch(...params), "Failed to get batch").pipe(
-      Effect.map((iterator) =>
-        Stream.fromAsyncIterable(
-          iterator,
-          (error) =>
-            new NATSError.JetStreamDirectStreamAPIError({
-              reason: "An error occurred in getBatch async iterable",
-              cause: error
-            })
-        )
-      ),
-      Stream.unwrap,
-      Stream.map(JetStreamStoredMessage.make)
+      Effect.map(NATSQueuedIterator.make(NATSError.JetStreamDirectStreamAPIError))
     ),
   getLastMessagesFor: (...params: Parameters<JetStream.DirectStreamAPI["getLastMessagesFor"]>) =>
     wrapAsync(() => direct.getLastMessagesFor(...params), "Failed to get last messages for subjects").pipe(
-      Effect.map((iterator) =>
-        Stream.fromAsyncIterable(
-          iterator,
-          (error) =>
-            new NATSError.JetStreamDirectStreamAPIError({
-              reason: "An error occurred in getLastMessagesFor async iterable",
-              cause: error
-            })
-        )
-      ),
-      Stream.unwrap,
-      Stream.map(JetStreamStoredMessage.make)
+      Effect.map(NATSQueuedIterator.make(NATSError.JetStreamDirectStreamAPIError))
     ),
 
   direct
